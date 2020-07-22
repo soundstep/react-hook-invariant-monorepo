06596e4cbe77443f93bc935f56545a17ae037e3a
Romuald Quantin <romu@soundstep.com>
2020-07-22 10:37:44 +0100

    Added swipeable.

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+import { useSwipeable } from 'react-swipeable'
+
+const Header = () => {
+
+    const handlers = useSwipeable({
+        onSwiped: (eventData) => {
+            console.log(eventData);
+        }
+    })
+
+    return (
+        <header {...handlers} className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    );
+};
+
+export default Header;
